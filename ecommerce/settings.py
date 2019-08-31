62d49ba13966ba07e6769793be9c8dd0cda29e15
Ubuntu <ubuntu@ip-172-31-45-59.us-east-2.compute.internal>
2019-08-31 17:58:10 +0000

    STRIPE CREDS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -130,8 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'))
+    os.path.join(BASE_DIR, "static"),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
